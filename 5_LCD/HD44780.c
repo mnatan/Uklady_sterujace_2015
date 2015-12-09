9255c7f1ff6f9fb5f884d2811ac94c458dbd3409
Marcin Natanek <natanek.marcin@gmail.com>
2015-12-09 13:26:20 +0100

    added template files

---
+++ 5_LCD/HD44780.c
@@ -0,0 +1,38 @@
+#include "HD44780.h"
+
+
+
+void WriteNibble(unsigned char nibbleToWrite)
+{
+
+}
+
+
+void WriteByte(unsigned char dataToWrite)
+{
+	
+}
+
+
+void LCD_Command(unsigned char)
+{
+};
+
+void LCD_Text(char *)
+{
+};
+void LCD_GoToXY(unsigned char, unsigned char)
+{
+};
+
+void LCD_Clear(void)
+{
+};
+
+void LCD_Home(void)
+{
+};
+
+void LCD_Initalize(void)
+{
+};
